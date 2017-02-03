572c162aa325c12c5548d35d0827769326ceeef5
buttegab <gabriel.butterick@students.olin.edu>
2017-02-03 16:28:15 -0500

    working obstacel avoider, requires modofied wall_follower_helper.py to function

--- warmup_project/scripts/obstacle_avoider.py
+++ warmup_project/scripts/obstacle_avoider.py
@@ -14,6 +14,7 @@ class ObstacleAvoider:
   def __init__(self):
     self.twist = Twist()
     self.twist.linear.x = 1
+    print("initializing forward")
     self.running_total = 0
     self.previous_rot = 'default'
     self.x = 0
@@ -30,23 +31,26 @@ class ObstacleAvoider:
     self.laser_sub = rospy.Subscriber('/stable_scan', LaserScan, self.process_scan)
 
     self.pub.publish(self.twist)
+    print("publishing forward")
 
   def process_odom(self, msg):
+    #needs to be updated for use in obstacle avoidance
     pose = msg.pose.pose
     self.x, self.y, self.rot = self.convert_pose_to_xy_and_theta(pose)
-    temp = 'jump'
+    print(self.rot)
+    # temp = 'jump'
 
-    if self.previous_rot == 'default':
-      self.previous_rot = self.rot
-    if (self.previous_rot > 0) and (self.rot < 0):
-      temp = self.rot
-      self.rot += 2*pi
+    # if self.previous_rot == 'default':
+    #   self.previous_rot = self.rot
+    # if (self.previous_rot > 0) and (self.rot < 0):
+      # temp = self.rot
+      # self.rot += 2*pi
 
-    self.rot_error = abs(self.rot - self.previous_rot)
-    self.running_total += self.rot_error
-    if temp != 'jump':
-      self.rot = temp
-    self.previous_rot = self.rot
+    # self.rot_error = abs(self.rot - self.previous_rot)
+    # self.running_total += self.rot_error
+    # if temp != 'jump':
+      # self.rot = temp
+    # self.previous_rot = self.rot
 
   def process_scan(self, msg):
     self.ranges = msg.ranges
@@ -66,9 +70,16 @@ class ObstacleAvoider:
     self.set_vals(spin=.1)
 
   def make_a_turn(self, direction):
-    while self.running_total < (pi/2 - self.rot_error/2):
-      self.set_vals(spin=.1*direction)
-    
+    direction = 1
+    print("making a turn")
+    # while self.running_total < (pi/2 - self.rot_error/2):
+    if direction > 0:
+      while self.rot < 1.5:
+        self.set_vals(spin=.1*direction)
+    else:
+      while self.rot > -1.5:
+        self.set_vals(spin=.1*direction)
+
     self.running_total = 0
     self.go_forward()
     if direction > 0:
@@ -77,8 +88,9 @@ class ObstacleAvoider:
       side = 90
     while self.ranges[side] < self.avoid_dist:
       self.wf.do_the_thing()
-    while abs(self.rot) > self.rot_error/2:
-      self.set_vals(spin=-.1*direction)
+    while self.rot > .05 or self.rot < -.05:
+      print("leaving wall following")
+      self.set_vals(spin=-.4*(self.rot/abs(self.rot)))
     self.go_forward()
 
   def convert_pose_to_xy_and_theta(self, pose):
@@ -87,14 +99,18 @@ class ObstacleAvoider:
       return (pose.position.x, pose.position.y, angles[2])
 
   def do_the_thing(self):
+    print("forward, main")
     self.go_forward()
 
     cone_ranges = self.ranges[-30:] + self.ranges[:30]
-    in_range_num = len([x for x in cone_ranges if x < 1])
-    if (in_range_num > 0) and (cone_ranges[0] == 0):
-      self.make_a_turn(1)
-    elif (in_range_num > 0) and (cone_ranges[-1] == 0):
+    in_range_num = len([x for x in cone_ranges if x < 1 and not x==0]) #dude what x<1 means even 0 "no response" counts
+    # if (in_range_num > 0) and (cone_ranges[-1] == 0):
+    #   self.make_a_turn(-1)
+    # elif (in_range_num > 0) and (cone_ranges[0] == 0):
+    #   self.make_a_turn(1)
+    if in_range_num >0 :
       self.make_a_turn(-1)
+
 
     self.pub.publish(self.twist)
 
---
+++ warmup_project/scripts/wall_follower_helper.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Header
+from sensor_msgs.msg import LaserScan
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Pose, Point, Vector3
+
+class FollowWall:
+  def __init__(self):
+    self.twist = Twist()
+
+    self.fore = 0
+    self.aft = 0
+    self.previous_fore = 0
+    self.previous_aft = 0
+    self.pls_stop = 1
+    self.current_marker_id = 0
+    self.fore_x = self.fore*(2**(-.5))
+    self.fore_y = -1*self.fore*(2**(-.5))
+    self.aft_x = -1*self.aft*(2**(-.5))
+    self.aft_y = -1*self.aft*(2**(-.5))
+
+    # rospy.init_node('wall_follower')
+    self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    self.marker_pub = rospy.Publisher('/visualization_messages/Marker', Marker, queue_size=10)
+    self.sub = rospy.Subscriber('/stable_scan', LaserScan, self.process_scan)
+
+  def process_scan(self, msg):
+    fore_deg = 315
+    aft_deg = 225
+    self.previous_fore = self.fore
+    self.previous_aft = self.aft
+    self.fore = msg.ranges[fore_deg]
+    self.aft = msg.ranges[aft_deg]
+    self.pls_stop = msg.ranges[0]
+
+    self.fore_x = self.fore*(2**(-.5))
+    self.fore_y = -1*self.fore*(2**(-.5))
+    self.aft_x = -1*self.aft*(2**(-.5))
+    self.aft_y = -1*self.aft*(2**(-.5))
+
+  def create_marker(self, x=0, y=0):
+    scale = Vector3(x=.1, y=.1, z=.1)
+    pt = Point(x=x, y=y)
+    pose = Pose(position=pt)
+    mark = Marker(type=2, pose=pose, scale=scale)
+    mark.color.a = 1
+    mark.color.b = 1
+    mark.color.r = .5
+    mark.color.g = .5
+    mark.header.frame_id = "base_link"
+    mark.id = self.current_marker_id
+    self.current_marker_id += 1
+    return mark
+
+  def set_vals(self, speed=0, spin=0):
+    self.twist.linear.x = speed; self.twist.linear.y = 0; self.twist.linear.z = 0
+    self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = spin
+
+  def do_the_thing(self):
+    print("wall following")
+    wall_dist_sum = 2.6
+    dist_sum = self.fore + self.aft
+    previous_dist_sum = self.previous_fore + self.previous_aft
+    spin_factor = 1.2
+
+    self.set_vals(speed=1, spin=spin_factor*(wall_dist_sum - dist_sum))
+
+    if self.aft and not self.fore:
+      self.set_vals(speed=1, spin=-1*spin_factor*(wall_dist_sum - dist_sum))
+    elif not (self.fore or self.aft):
+      self.set_vals(speed=1, spin=spin_factor*(wall_dist_sum - previous_dist_sum))
+      if not (self.previous_fore or self.previous_aft):
+        self.set_vals(speed=.1)
+
+    if (self.pls_stop < 1) and (self.pls_stop != 0):
+      self.set_vals(spin=.5)
+    elif (self.twist.linear.x > .2) and self.fore and self.aft:
+      self.marker_pub.publish(self.create_marker(self.fore_x, self.fore_y))
+      self.marker_pub.publish(self.create_marker(self.aft_x, self.aft_y))
+
+    # print self.twist.linear.x
+    self.pub.publish(self.twist)
+
+if __name__ == "__main__":
+  fw = FollowWall()
+  while not rospy.is_shutdown():
+    fw.do_the_thing()
