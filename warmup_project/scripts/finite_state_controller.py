54788a62a74c87ff7991060bb55cd3cdd4f1a8a3
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-02-05 21:31:39 -0500

    adds a kinda cruddy fsc

---
+++ warmup_project/scripts/finite_state_controller.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+""" Print distance to object immediately in front of the robot """
+
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Header
+from neato_node.msg import Bump
+import rospy
+
+class FiniteStateController:
+  def __init__(self):
+    self.bumped = 0
+    rospy.init_node('e_stop')
+    self.sub = rospy.Subscriber('/bump', Bump, self.process_bump)
+    self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    self.twist = Twist()
+    self.twist.linear.x = 1
+    self.do_the_thing()
+    print "Node is finished!"
+
+  def process_bump(self, msg):
+    self.bumped = msg.leftFront + msg.rightFront + msg.leftSide + msg.rightSide
+
+  def set_vals(self, speed=0, spin=0):
+    self.twist.linear.x = speed; self.twist.linear.y = 0; self.twist.linear.z = 0
+    self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = spin
+    self.pub.publish(self.twist)
+
+  def do_the_thing(self):
+    if self.bumped:
+      # deer in headlights
+      self.set_vals()
+    else:
+      # freak out
+      self.set_vals(speed=.5, spin=.5)
+    self.pub.publish(self.twist)
+
+if __name__ == "__main__":
+  fsc = FiniteStateController()
+  while not rospy.is_shutdown():
+    fsc.do_the_thing()
