42d0ab9024d6d2e8cc62429e97f1ccc08ed22bfa
buttegab <gabriel.butterick@students.olin.edu>
2017-02-15 22:56:09 -0500

    third

--- warmup_project/scripts/teleop.py
+++ warmup_project/scripts/teleop.py
@@ -8,11 +8,13 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Header
 import rospy
 
+#initialize node and begin publishing to /cmd_vel to cause movement
 rospy.init_node('better_teleop')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 twist = Twist()
 
 def getKey():
+    """Provided function that waits for and returns key presses as they happen"""
     tty.setraw(sys.stdin.fileno())
     select.select([sys.stdin], [], [], 0)
     key = sys.stdin.read(1)
@@ -22,6 +24,7 @@ def getKey():
 settings = termios.tcgetattr(sys.stdin)
 key = None
 
+#while the exit character is not pressed, take basic instruction on how the robot should move.
 while key != '\x03':
     key = getKey()
     
