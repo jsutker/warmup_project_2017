72b16f3cb14412d07fef665110acee70130912d1
buttegab <gabriel.butterick@students.olin.edu>
2017-02-15 23:12:30 -0500

    this is all im doing for now

--- warmup_project/scripts/person_follower_helper.py
+++ warmup_project/scripts/person_follower_helper.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+"""
+The person follower code uses the laser scanner to detect the nearest object within a specified distance and 
+focusses on that object. If the object moves, the robot makes an effort to keep the object relatively in 
+front of the robot and within a certain distance of the front of the robot. When it can't find anything in 
+the surrounding area close enough to focus on, it spins in a circle idly.
+"""
+
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Header
 from sensor_msgs.msg import LaserScan
@@ -7,8 +14,10 @@ import rospy
 
 class PersonFollower:
   def __init__(self):
+    #robot has two states, wait and follow
     self.current_state = "wait"
     self.cone_width = 40 # if odd, truncates to self.cone_width - 1
+    #determine which degrees of laser scanner are relevant for the focusing point
     self.cone_left  = -1*(self.cone_width/2)
     self.cone_right = (self.cone_width/2) + 1
     self.perim_dist = 1.0
@@ -19,7 +28,7 @@ class PersonFollower:
     self.nearest_deg = 0
     self.nearest_deg_dist = 0
 
-    # rospy.init_node('person_follow')
+    #instantiate rospy node and subscriptions/publishings
     self.sub = rospy.Subscriber('/scan', LaserScan, self.process_scan)
     self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     self.twist = Twist()
@@ -27,13 +36,19 @@ class PersonFollower:
     print "Node is finished!"
 
   def process_scan(self, msg):
+    #make the scans accessible elsewhere in the class
     self.ranges = msg.ranges
 
   def set_vals(self, speed=0, spin=0):
+    """Helper function designed to take in a speed and angular velocity and set the appropriate values
+    in the twist for later publishing."""
     self.twist.linear.x = speed; self.twist.linear.y = 0; self.twist.linear.z = 0
     self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = spin
 
   def wait(self):
+    """When in the waiting state, the robot waits for an object to move within range and continues scanning
+    until that happens.
+    """
     self.set_vals(spin=.2)
     nearest_deg = 0
     nearest_deg_dist = self.perim_dist + 1
@@ -47,6 +62,10 @@ class PersonFollower:
       self.current_state = "follow"
 
   def follow(self):
+    """If the robot is in the follow state, the robot attempts to follow the object it has noticed
+    to the best of its ability. It narrows the field of view it pays attention to and attempts to 
+    keep the object within that cone. When the object moves far away or closer, left or right, the
+    robot moves to maintain distance and focus on the object."""
     cone_ranges = self.ranges[self.cone_left:] + self.ranges[:self.cone_right]
     nearest_deg = 0
     nearest_deg_dist = self.follow_max + 1
@@ -63,6 +82,7 @@ class PersonFollower:
       self.current_state = "wait"
 
   def center(self, degree=0, speed=0):
+    """Centers the robot on whatever is closest to it."""
     if degree == 0:
       sign = 1
     else:
@@ -72,6 +92,8 @@ class PersonFollower:
     self.set_vals(speed=speed, spin=spin)
 
   def do_the_thing(self):
+    """Main function that regulates which state based function is being called and publishes the resulting
+    twist information."""
     # r = rospy.Rate(2)
     if self.current_state == "wait":
       self.wait()
